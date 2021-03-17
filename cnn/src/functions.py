418e9d520e2e3bd6711a8a651add06f6c5a9fc0a
Sergio <sslp@cin.ufpe.br>
2021-03-17 18:53:18 -0300

    cnn mnist with parameter tuning

--- cnn/src/functions.py
+++ cnn/src/functions.py
@@ -1,18 +1,20 @@
 import keras
 from keras.optimizers import SGD
 
-def build_model(hidden_layer_size, learning_rate, activation):
-    model = keras.models.Sequential() #sequential -> modelo do keras composto por uma pilha de camadas conectadas
-
-    #primeira camada (Flatten) -> apenas faz o preprocessamento das imagens, convertando cada uma em um array de uma dimensão
-    model.add(keras.layers.Flatten(input_shape=[28, 28])) 
-
-    #cada camada Dense abaixo vai ter seu próprio vetor de pesos entre os neurons e os inputs
-    #segunda camada -> hidden layer com 300 neurons, função de ativação ReLU
-    model.add(keras.layers.Dense(hidden_layer_size, activation=activation))
-
-    #output layer -> 10 neurons (numero de saídas) usando softmax (classes exclusivas e classificaçao p mais de duas classes), se fosse binario, usaria sigmoid
-    model.add(keras.layers.Dense(10, activation="softmax"))
+def build_model(learning_rate, n_filters):
+    model = keras.models.Sequential([
+      keras.layers.Conv2D(n_filters, 7, activation="relu", padding="same", input_shape=(28, 28, 1)),
+      keras.layers.MaxPooling2D(2),
+      keras.layers.Conv2D(n_filters*2, 3, activation="relu", padding="same"),
+      keras.layers.Conv2D(n_filters*2, 3, activation="relu", padding="same"),
+      keras.layers.MaxPooling2D(2),
+      keras.layers.Flatten(),
+      keras.layers.Dense(n_filters*2, activation="relu"),
+      keras.layers.Dropout(0.5),
+      keras.layers.Dense(n_filters*2, activation="relu"),
+      keras.layers.Dropout(0.5),
+      keras.layers.Dense(10, activation="softmax")
+    ])
 
     custom_optimizer = SGD(lr = learning_rate)
 
